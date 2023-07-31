1c41c73d26bb3062eeeedd52e7b987d2a828107e
manueloid <manuel.odelli@gmail.com>
2023-07-31 17:54:21 +0100

    Need to rethink the whole process
    I found that I can rewrite the product of the Hermite polynomial and the
    Gaussian term in a different fashion, so I need to rethink the whole
    thing.
    I will do that in a different location, as this package is reserved to
    numerical implementation.

--- test/wavefunction_tests/fourier_checks.jl
+++ test/wavefunction_tests/fourier_checks.jl
@@ -22,7 +22,7 @@ $$ i^n \sqrt{\alpha} \exp\left\{\frac { -\alpha x^2}{2} \right\} \gamma^n \mathc
 where
 $ \gamma = \sqrt{2 \alpha \beta^2 - 1} $
 
-I also will define the Fourier transform of a function depending on the variabl $p$ as
+I also will define the Fourier transform of a function depending on the variable $p$ as
     $$ F^{-1}[f(p)](z) =\frac{1}{\sqrt{2\pi h}} \int_{-\infty}^{\infty} f(p) \exp\left\{\frac{ipz}{h}\right\} dp $$
 
 For the moment I will only focus on the product of the Gaussian and the hermite polynomial with general $\alpha$ and $\beta$, and only later I will try to map the two parameters with the respective functions in the argument of theSTA wave function.
@@ -40,57 +40,76 @@ $$ \frac{\sqrt{\xi_0}}{\sqrt[4]{\pi }
  e^{-p^2/2\alpha} \mathcal{H}_n(\beta p) $$
 
  with $\alpha$ and $\beta$ to be determined.
+
+For the moment I will only focus on the spatial term of the product of the Gaussian term and the Hermite polynomial, I will then check if the normalisation and the orthogonality hold and finally I will define the analytic solution of the Fourier transform of the product and test the results.
+Again, the Gaussian and the Hermite polynomial will be in terms of the two parameters $\alpha$ and $\beta$.
 =#
 
 """
-    `time_dependent(n,t, c::Control)`
-    Return the solely time dependent part of the STA wave function in momentum representation.
+    `spatial(n::Int64, p::Float64, α::ComplexF64, β::Float64)`
+Return the product of the Gaussian function and the Hermite polynomial in momentum representation and in terms of the parameters `α` and `β`.
 """
-function time_dependent(n, t, c::Control)
-    ξ0, U = EBJJ.scaling_ξ0(c), c.U # constants
-    b(t) = auxiliary(t, c) # Auxiliary function
-    normalisation = sqrt(EBJJ.scaling_ξ0(c)) / (π)^0.25 # Normalisation factor
-    itp = EBJJ.interpolation_integral(c) # Interpolating function for the integral of the phase
-    imaginary_phase(n, t) = exp(-im * (n + 0.5) * ξ0^2 * U / 2 * itp(t)) # Imaginary phase
-    return normalisation * imaginary_phase(n, t) / sqrt(2^n * factorial(n) * b(t))
+function spatial(n::Int64, p::Float64, α::ComplexF64, β::Float64)
+    return exp(-p^2 / (2 * α^2)) * SpecialPolynomials.basis(Hermite, n)(β * p)
 end
 """
-    `spatial(n, p, α, β)`
-Return the solely spatial part of the STA wave function in momentum representation in terms of the parameters α and β.
+    `analytic(n::Int64, x::Float64, α::ComplexF64, β::Float64)`
+Return the analytic solution of the Fourier transform of the product of the Gaussian function and the Hermite polynomial in position representation and in terms of the parameters `α` and `β`.
 """
-function spatial(n, p, α, β)
-    gaussian(p, α) = exp(-p^2 / (2 * α)) # Gaussian function
-    hermite(n, p, β) = SpecialPolynomials.basis(Hermite, n)(β * p) # Hermite polynomial
-    return gaussian(p, α) * hermite(n, p, β)
-end
-"""
-    `analytical_solution(n, x, t, α, β)`
-    Return the analytical solution of the Fourier transform of the product of a Gaussian function and a Hermite polynomial, where the argument of the two functions are two complex numbers `α` and `β`.
-"""
-function analytical_solution(n, x, h, α, β)
-    γ = sqrt(Complex(2 * α * β^2 - 1)) # Auxiliary variable
-    return im^n * sqrt(Complex(β)) * exp(-α * x^2 / 2) * γ^n * SpecialPolynomials.basis(Hermite, n)(α * β / γ * x)
-end
-"""
-    `fourier_transform(x, h, f::Function)`
-Give the Fourier transform of a function `f` depending on the variable `p` with constant factor `h`.
-"""
-function fourier_transform(x, h, f::Function)
-    return quadgk(p -> f(p) * exp(-im * p * x / h), -10.0, 10.0, rtol=1e-3)[1] / sqrt(2 * π * h)
+analytic(n) = n
+
+@testset "Testing normalisation momentum for α and β" begin
+    α = 1.0 + 0.0 * im
+    β = 1.0
+    @test quadgk(
+        x -> abs(
+            spatial(2, x, 1.0 + 0.0 * im, 1.0) *
+            conj(spatial(0, x, 1.0 + 0.0 * im, 1.0))
+        ), -Inf, Inf)[1] ≈ sqrt(π)
 end
 
-@testset "Fourier check, general α, β" begin
-    α, β = 1.2, 0.1 # Parameters
-    n = 2
-    c = ControlFull()
-    h = 1 / c.N
-    t = [0.0, c.T / 2, c.T]
-    full(p) = time_dependent(n, t[1], c) * spatial(n, p, α, β)
-    analytic(x) = analytical_solution(n, x, h, α, β)
-    xrange = range(-1, 1, length=2)
-    full_x = [quadgk(p -> full(p) * exp(im * p * x), -10.0, 10.0, rtol=1e-3)[1] / sqrt(2 * π) for x in xrange]
-    @test full_x ≈ analytic.(xrange)
-    # @test analytic.(xrange) == 1.0
-    # @test Spatial.(xrange) ≈ analytic.(xrange)
-    # @test Full.(xrange) ≈ Spatial.(xrange)
-end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
