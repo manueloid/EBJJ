bee55a0ba229749229513aac391533f8be937c0c
manueloid <manuel.odelli@gmail.com>
2023-08-03 22:42:45 +0100

    Reran some tests
    Just checked if the tests I made for the STA wave function still work as I am ready to move to the STA wave function in position space

--- test/wavefunction_tests/fourier_checks.jl
+++ test/wavefunction_tests/fourier_checks.jl
@@ -7,42 +7,32 @@ using EBJJ
 
 #=
 # Fourier checks
-In this set of tests I am going to numerically check if the subsitutions I made to turn the STA wave function from momentum to position representation are correct.
+In this set of tests I am going to numerically check if the simplifications I made to turn the STA wave function from momentum to position representation are correct.
 
-My plan is to find make as much simplifications as possible, but in order to do that, I need to know if the substitutions I made are correct.
+I have already checked in [the relevant repository](~/Repos/ExternalBJJ/fourier_transform.wlnb) that the Fourier transform of the product of a Gaussian function and of a Hermite polynomial has a specific close form.
+
+Here I only want to check if the normalisation and the orthogonality hold.
+This can be done by considering the fact that the Fourier transform of the whole STA wave function in momentum representation is the product of the Fourier transform of the spatial part and of the time dependent part.
+
+Hence the STA wave function in position representation is just the product of the time dependent term and the Fourier transform of term dependent on the momentum varible $p$.
 
 Plans:
-I have already checked that the STA wf in monentum representation is correct, so I will use that as a reference.
-I will hence perform a numerical Fourier transform on the STA wf in momentum representation and then I will compare it with the analytical solution that I got from the internet.
-
 First, I will define the analytical solution of the Fourier transform of the product of a Gaussian function of the form $ e^{-p^2/2\alpha}$ and a hermite polynomial of the form $ \mathcal_n(\beta p) $
 
 It is given by:
 $$ i^n \sqrt{\alpha} \exp\left\{\frac { -\alpha x^2}{2} \right\} \gamma^n \mathcal{H}_n\left( \frac{\alpha\beta}{\gamma}x\right)$$
 where
-$ \gamma = \sqrt{2 \alpha \beta^2 - 1} $
+$ \gamma = \sqrt{2 \alpha^2 \beta^2 - 1} $
 
-I also will define the Fourier transform of a function depending on the variable $p$ as
-    $$ F^{-1}[f(p)](z) =\frac{1}{\sqrt{2\pi h}} \int_{-\infty}^{\infty} f(p) \exp\left\{\frac{ipz}{h}\right\} dp $$
+Then I will define the whole STA wave function in position representation and I will check if the normalisation and the orthogonality hold.
 
-For the moment I will only focus on the product of the Gaussian and the hermite polynomial with general $\alpha$ and $\beta$, and only later I will try to map the two parameters with the respective functions in the argument of theSTA wave function.
+I will start from the normalisation condition.
+I will check the normalisation for two different kinds of functions, that in theory should give the same result:
+1. the product of the STA wave function in position representation and its complex conjugate with no simplifations
+2. the product of the STA wave function in position representation and its complex conjugate with the simplifications already implemented in the function definition.
 
-I want to compare three quantities:
-- the analytical solution
-- the numerical solution obtained by using the Fourier transform on the full STA wave function in monentum representation
-- the numerical solution obtained by using the Fourier transform on only the spatial part of the STA wave function in monentum representation and then multiplying the time dependent one
-
-In the following then I will assume that the STA wave function in momentum representation is of the form
-
-$$ \frac{\sqrt{\xi_0}}{\sqrt[4]{\pi }
-\exp \left(-i \left(n+\frac{1}{2}\right) \int_0^t \frac{\xi_0^2 U}{2 b(\tau )^2} \, d\tau \right) 
-1/\sqrt{2^n n! b(t)}}
- e^{-p^2/2\alpha} \mathcal{H}_n(\beta p) $$
-
- with $\alpha$ and $\beta$ to be determined.
-
-For the moment I will only focus on the spatial term of the product of the Gaussian term and the Hermite polynomial, I will then check if the normalisation and the orthogonality hold and finally I will define the analytic solution of the Fourier transform of the product and test the results.
-Again, the Gaussian and the Hermite polynomial will be in terms of the two parameters $\alpha$ and $\beta$.
+For the moment I will just use the parameter $\alpha$ and $\beta$ to be complex numbers and I will check the normalisation for some fixed times.
+If everything works according to plan, I will then check if the same relation holds if I substitute $\alpha$ and $\beta$ with the respective time depending functions
 =#
 
 """
