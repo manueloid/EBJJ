d3b32162997a4b18d8db6a6d563191d18673afec
manueloid <manuel.odelli@gmail.com>
2023-08-14 09:59:27 +0100

    Completed generalised definitions
    All the definitions for the wavefunctions are now in place.
    The functions will generall take a complex or float argument and return
    the value of the wavefunction at that point.
    
    All is explained in the documentation.

--- src/wavefunction.jl
+++ src/wavefunction.jl
@@ -2,46 +2,69 @@
 # STA Wave function 
 
 I need to things slowly and carefully, and write tests along the way.
-In this file I will only focus on defining the wave function, in a different one I will actually evaluate the eSTA corrections.
-
-## 1. Constant factor and helpful functions
-
-First I will start by defining the constant factor $\xi_0$ and the functions $\alpha(t)$ and $\beta(t)$ that go into the Fourier transform of the STA wave function.
-The names of the function will be (respectively) `scaling_ξ0`, `gaussian_arg` and `hermite_arg`.
-I have the feeling that it will make more sense to define everything in one big function instead of defining the constants and the functions separately, as I think it will save some precompilation time.
-It will also make sense as I do not have to define the auxiliary function `b(t)` twice.
-I have already found out that in this case there is not much to simplify, so I will just write the integrand as it is.
+In this file I will only focus on defining the wave function and the functions that will go into the integrals, in a different one I will actually evaluate the eSTA corrections i.e. the integrals.
 
 I am going to use the fact that $ \alpha ^2 $ is the inverse of the argument of the Gaussian part in the STA wave function, and if we are going to call the argument $ \eta^2 $, the parameter $ \beta $ is nothing more than $ \sqrt{\Re{\eta^2}} $.
 
 Thus, we can simplify the term $ \alpha ^2 \beta ^2 -1 $ that appears twice in the Fourier transform of the product of the Gaussian term and the Hermite polynomial as $ \eta^{2*} / \eta^2 $
+
+I will first focus on the spatial term of the wave function and only then I will move on to the time dependent part, which is common to all the integrals and can thus be simplified greatly.
+
+Throughout this document, I will not define any function depeding on the actual values of the control parameters, I will only define functions that take general complex and real numbers as arguments.
+I will specialise the functions in the [relevant file](./src/corrections.jl), where I will also define the integrals.
+
+## 1. Spatial part
+
+For this term I will only need the product of the Gaussian term and the Hermite polynomial, where the only _tricky_ part is the argument of the Hermite polynomial, which is given by
+
+$$ \frac{\alpha^2 x^2}{\sqrt{2\alpha^2 \beta^2 - 1}} $$
+
+that can be rewritten with the formulation of $ \eta $ as
+
+$$ \frac{\sqrt{\Re[\eta^2]}}{\eta} \frac{1}{\gamma}  $$
+
+where $ \gamma = \sqrt{\eta^{2*} / \eta^2} $.
+
+### 1.1 General spatial part of the wave function and ground state
+
+Here I will define the general spatial part of the ground state of the STA wave function, as well as the ground state for it.
+The former is the lefthand side of the integral $ \langle \psi_n |\hat{H} | \psi_0 \rangle $, while the latter is the righthand side and it is only the gaussian term, the Hermite polynomial being one.
+
+#### 1.1.1 Code implementation
+
+I think it makes sense to define a function that takes only the energy level `n` and the position `z` as arguments as well as a general complex number `η`.
+I will then define the multiple dispatch versions of the function, where I can pass the arguments in any order.
+
+I would like to point out that `spatial_fourier` is not yet the complex conjugate of the spatial part, and it has to be implemented in due time.
 =#
 
 """
-    `spatial_fourier(n::Int64, t, z, η::ComplexF64)`
+    spatial_fourier(n::Int64, z, η::ComplexF64)
 Evaluate the analytic solution of the Fourier transform of the STA wave function for a complex number η, for a time `t` and at position `z`.
 The value η is nothing more than 1/α² in the notes, while β is the real part of η.
 """
-function spatial_fourier(n::Int64, z, η::ComplexF64)
+function spatial_fourier(n::Int64, z::Float64, η::ComplexF64)
     γ = sqrt(conj(η) / η) # this is the √α²β² - 1 factor
     num = sqrt(real(η)) / η # This is the numerator inside the Hermite polynomial
     return exp(-z^2 / (2 * η)) * SpecialPolynomials.basis(Hermite, n)(z * num / γ)
 end
-ground_state(z, η::ComplexF64) = spatial_fourier(0, z, η)
+
+# Multiple dispatch version of the previous function
+spatial_fourier(n::Int64, η::ComplexF64, z::Float64) = spatial_fourier(n, z, η)
+spatial_fourier(η::ComplexF64, n::Int64, z::Float64) = spatial_fourier(n, z, η)
+spatial_fourier(η::ComplexF64, z::Float64, n::Int64) = spatial_fourier(n, z, η)
+spatial_fourier(z::Float64, η::ComplexF64, n::Int64) = spatial_fourier(n, z, η)
+spatial_fourier(z::Float64, n::Int64, η::ComplexF64) = spatial_fourier(n, z, η)
+
+# Ground state of the STA wave function multiply dispatched
+ground_state(z::Float64, η::ComplexF64) = exp(-z^2 / (2 * η))
+ground_state(z::Float64, η::ComplexF64) = spatial_fourier(0, z, η)
+ground_state(η::ComplexF64, z::Float64) = ground_state(z, η)
 
 #=
-### 1.1 Integrand depdending on the $ b_h(z) $ function.
+Since the part of the integrand depending on the $ b_h $ factor does not have any simplification that can be carried out, there is no need to define it here.
+This will be done in the [relevant corrections file ](./src/corrections.jl).
 
-I will start by defining a function that given (among other things) the time `t` and a position `x` returns the value of the integrand 
-$$
-\psi_{n}^{*}(t,x)   \left[ e^{-i\hat{p}} b_{h}(z) + b_{h}(z)e^{i\hat{p}} \right] \psi_{0}(t,x)
-$$
-where $ \psi_n(t,z) is the spatial part of the STA wave function.
-=#
-
-scaling_ξ0(c::Control) = sqrt(8c.J0 * c.N / c.U)
-
-#=
 ### 1.2 Integrand depending on the second derivative of the ground state
 
 In this section we are going to evaluate the integral of the form 
@@ -50,50 +73,29 @@ $$ \langle \psi_n | \frac{d^2}{dz^2} | \psi_0 \rangle $$.
 This integral is non zero if $ n $ is even.
 
 Moreover, I could analytically take the second derivative of the ground state, to get
-$$\alpha ^2 e^{-\frac{1}{2} \alpha ^2 x^2}\left( \alpha^2 x^2 - 1\right)$$
+$$
+    \alpha ^2 e^{-\frac{1}{2} \alpha ^2 x^2}\left( \alpha^2 x^2 - 1\right)
+$$
+
+which - in the $ \eta $ formulation - is given by
+$$
+    \frac{e^{-\frac{z^2}{2 \eta ^2}} \left(z^2-\eta ^2\right)}{\eta ^4}
+$$
 
 we can thus obtain a single function to evaluate the integrand, which is given by
-    $$
-        \mathcal{H}_{n}^{*}\left(\frac{\alpha^{2}\betax}{\sqrt{2\alpha^{2}\beta^{2} - 1}}\right)
-       \alpha^2  \exp{- \frac{x^2}{2} \Re{\alpha^2}} \left( \alpha^2 x^2 - 1\right)
-    $$
+$$
+    \mathcal{H}_{n}^{*}\left(\frac{\alpha^{2}\beta x}{\sqrt{2\alpha^{2}\beta^{2} - 1}}\right)
+    \alpha^2  \exp{- \frac{x^2}{2} \Re{\alpha^2}} \left( \alpha^2 x^2 - 1\right)
+$$
 
 #### 1.2.1 Code implementation
 
-I am going to write a function that returns the value of this integrand given the time `t` and the position `z`.
-I do not think there is any need to split the integral into two addends.
+Here I am going to only define the function that returns the value of the second derivative of the ground state, I will not define the whole integrand function as I will do that in the [relevant file](./src/corrections.jl).
+The function will take only a complex number `η` and a position `z` as arguments, and it will return a complex number.
 
-On the other hand though, I am not going to reuse any of the functions I used to calculate the part depending on the $ b_h $ factor, as there are more simplifications I can carry out in this section.
-
-I do not know if there is any need to implement the multiple dispatched versions of the functions, but I am going to leave them there just in case.
-
-# Multiple dispatched version of the previous functions
-
-"""
-    `bh_integrand(n::Int64, t, z, c::Control)`
-Evaluate the integrand of the Fourier transform of the STA wave function for a time `t` and at position `z` given the energy level `n`
-This is the multiple dispatched version of the previous one.
-"""
-function bh_integrand(n::Int64, t, z, c::Control)
-    ξ0, U, h = scaling_ξ0(c), c.U, 1 / c.N # Definition of the constants
-    b(t) = auxiliary(t, c) # Auxiliary function
-    db(t) = ForwardDiff.derivative(b, t) # Derivative of the auxiliary function
-    η(t) = ξ0^2 / b(t)^2 - 2im * db(t) / (U * b(t))
-    return bh_integrand(n, z, h, η(t))
-end
-"""
-    `sd_integrand(n::Int64, t, z, c::Control)`
-Evaluate the integrand of the Fourier transform of the STA wave function for a time `t` and at position `z` given the energy level `n`, for a control parameter `c`.
-This is the multiple dispatched version of the previous one.
-"""
-function sd_integrand(n::Int64, t, z, c::Control)
-    ξ0, U = scaling_ξ0(c), c.U # Definition of the constants
-    b(t) = auxiliary(t, c) # Auxiliary function
-    db(t) = ForwardDiff.derivative(b, t) # Derivative of the auxiliary function
-    η(t) = ξ0^2 / b(t)^2 - 2im * db(t) / (U * b(t))
-    return sd_integrand(n, z, η(t))
-end
 =#
+sd_groundstate(z::Float64, η::ComplexF64) = (z^2 - η^2) * exp(-z^2 / (2 * η^2)) / η^4
+sd_groundstate(η::ComplexF64, z::Float64) = sd_groundstate(z, η)
 
 #=
 ## 2. Exclusively time dependent part
@@ -102,7 +104,7 @@ By what I have already described in a [different file](~/Repos/ExternalBJJ/Docum
 This is
 
 $$
-    \sqrt{\frac{\Re[\eta^2]}{\pi}} % normalisation not depending on the excitation
+\sqrt{\frac{\Re[\eta^2]}{\pi}} \frac{1}{\Re[\eta^2]} % normalisation not depending on the excitation
 	\frac{-i^n \gamma^{n}}{\sqrt{2^{n}n!}} % normalisation term depending on the excitation
 	\exp\left\{i n \int_{0}^{t} \frac{\xi_{0}^{2} U }{2 b(\tau)^{2}} d\tau \right\} % imaginary time evolution factor
     tag{1}
@@ -136,10 +138,11 @@ I can thus use the multiple dispatch where I can pass only one argument, and one
 I need to remember that the name of the variable `\eta` can be a little misleading, as it actually not the complex number $\eta$ but its square.
 Moreover, I will define the second normalisation function as taking `γ` as an argument, as it makes the code more readable.
 
+Again, the remember that 
 =#
 
 # First part of the normalisation
-normalisation(reη::Float64) = sqrt(reη) / sqrt(pi)
+normalisation(reη::Float64) = sqrt(reη) / sqrt(pi) / reη^2
 normalisation(η::ComplexF64) = normalisation(real(η))
 
 # Second part of the normalisation, the one with the excitation energy
@@ -149,6 +152,19 @@ normalisation(γ::ComplexF64, n::Int64) = normalisation(n, γ)
 # Imaginary phase
 imaginary_phase(n::Int64, k::Float64) = exp(im * n * k)
 imaginary_phase(k::Float64, n::Int64) = imaginary_phase(n, k)
+
+# Full function
+function time_dependent(n::Int64, η::ComplexF64, k::Float64)
+    γ = sqrt(conj(η) / η)
+    return normalisation(η) * normalisation(n, γ) * imaginary_phase(n, k)
+end
+
+# Multiple dispatch combinations
+time_dependent(n::Int64, k::Float64, η::ComplexF64) = time_dependent(n, η, k)
+time_dependent(k::Float64, η::ComplexF64, n::Int64) = time_dependent(n, η, k)
+time_dependent(k::Float64, n::Int64, η::ComplexF64) = time_dependent(n, η, k)
+time_dependent(η::ComplexF64, k::Float64, n::Int64) = time_dependent(n, η, k)
+time_dependent(η::ComplexF64, n::Int64, k::Float64) = time_dependent(n, η, k)
 
 #=
 #### 2.1.1 Imaginary phase
@@ -164,7 +180,7 @@ Finally, I chose not to define the final imaginary phase function, I will define
 =#
 
 """
-    `interpolation_integral(tf::Float64, b::Function64; npoints=1000)`
+    interpolation_integral(tf::Float64, b::Function64; npoints=1000)
 Return a function which is the interpolation of the function f(t) = ∫₀ᵗ b(τ) dτ in the interval [0, tf].
 The function takes the argument `npoints` which is the number of points used for the interpolation. The default value is 1000 as it is the best trade-off between speed and accuracy.
 """
