3f7758a19c99e2da3c2547d92f78fb41da738350
manueloid <manuel.odelli@gmail.com>
2023-08-15 09:35:57 +0100

    Added the `src/packages.jl` file to the repository.
    This file will help me testing and developing the package inside the
    code I am writing.
    
    Just include this file will load all the packages I need to test the
    functionalties I am developing.

---
+++ src/packages.jl
@@ -0,0 +1,13 @@
+# This file is used to load all the packages used in the code
+using EBJJ
+using Revise # This is just used during development
+using Cthulhu # This is used to speed up the code
+using BenchmarkTools # This is used to benchmark the code
+
+# Packages used in the code
+using ForwardDiff
+using QuadGK
+using Interpolations
+using SpecialPolynomials
+
+
