93e09d3c1be6b0463887e92749779f9d1986b8fc
manueloid <manuel.odelli@gmail.com>
2023-08-30 22:54:32 +0100

    Started testing each part
    I started testing each part of the STA wave function, to see if the
    simplified version and the original one agree.
    The normalisation part seems to be working at the moment.

--- test/corrections_test.jl
+++ test/corrections_test.jl
@@ -12,6 +12,23 @@ I will thus start by using the function definition that takes into account the p
 =#
 
 """
+    argument(t, c::Control)
+Return the argument of the Gaussian part of the STA wave function at a time `t` given the control parameter `c`.
+"""
+function argument(t, c::Control)
+    J0, N, U = c.J0, c.N, c.U
+    b(t) = auxiliary(t, c) # Auxiliary function
+    db(t) = ForwardDiff.derivative(b, t) # Derivative of the auxiliary function
+    α(t::Float64) = 2 / b(t)^2 * sqrt(2J0 * N / U) - im * db(t) / (U * b(t)) # Parameter of the Gaussian term
+    return α(t)
+end
+normalisation_wh(n::Int64, η::ComplexF64) = (real(η) / pi)^(1 / 4) / sqrt(2^n * factorial(n)) * im^n * sqrt(conj(η) / η)^n / sqrt(η)
+normalisation_wh(n::Int64, t::Float64, c::Control) = normalisation_wh(n, argument(t, c))
+
+gaussian_wh(z::Float64, η::ComplexF64) = exp(-z^2 / (2 * η))
+hermite_wh(n::Int64, z::Float64, η::ComplexF64) = SpecialPolynomials.basis(Hermite, n)(z * sqrt(real(η)) / abs(η))
+spatial_fourier_wh(n::Int64, z::Float64, η::ComplexF64) = normalisation(η, n) * gaussian(z, η) * hermite(n, z, η)
+"""
      wave_function(n::Int64, t, x, c::Control)
 Return a general STA wave function for excitation `n` at time `t` and position `z`.
 This function is helpful to debug code but is not the best when it comes to performances
@@ -23,11 +40,14 @@ function wave_function(n::Int64, t, x, c::Control)
     α(t::Float64) = 2 / b(t)^2 * sqrt(2J0 * N / U) - im * db(t) / (U * b(t)) # Parameter of the Gaussian term
     imag_phase_integrand(t::Float64) = sqrt(2J0 * N * U) / b(t)^2
     φ(t::Float64) = quadgk(τ -> imag_phase_integrand(τ), 0.0, t)[1]
-    return exp(-im * (n + 1 / 2) * φ(t)) * spatial_fourier(n, x, α(t))
+    return exp(-im * (n + 1 / 2) * φ(t)) * spatial_fourier_wh(n, x, α(t))
 end
-ground_state(n::Int64, t, x, c::Control) = wave_function(n, t, x, c)
-pairing(n::Int64, t, x, c::Control) = wave_function(n, t, x, c) * conj(wave_function(n, t, x, c))
-normalisation(η::ComplexF64, n::Int64) = (real(η) / pi)^(1 / 2) / sqrt(2^n * factorial(n)) * (-im)^n * sqrt(conj(η) / η)^n / abs(η)
+
+ground_state(t, x, c::Control) = wave_function(0, t, x, c)
+pairing(n::Int64, t, x, c::Control) = ground_state(t, x, c) * conj(wave_function(n, t, x, c))
+normalisation(n::Int64, η::ComplexF64) = (real(η) / pi)^(1 / 2) / sqrt(2^n * factorial(n)) * (-im)^n * sqrt(conj(η) / η)^n / abs(η)
+normalisation(n::Int64, t, c::Control) = normalisation(n, argument(t, c))
+
 gaussian(z::Float64, η::ComplexF64) = exp(-z^2 / (2 * η))
 hermite(n::Int64, z::Float64, η::ComplexF64) = SpecialPolynomials.basis(Hermite, n)(z * sqrt(real(η)) / abs(η))
 spatial_fourier(n::Int64, z::Float64, η::ComplexF64) = normalisation(η, n) * gaussian(z, η) * hermite(n, z, η)
@@ -47,8 +67,8 @@ function simplified(n::Int64, t, x, c::Control)
     return exp(im * n * φ(t)) * spatial_fourier(n, x, αc(t)) * gaussian(x, α(t))
 end
 
-n = 3
-t, z = rand(Float64), rand(Float64)
+n = 0
 c = ControlFull()
-pairing(n, t, z, c)
-simplified(n, t, z, c)
+t, z = rand(Float64) * c.T, rand(Float64)
+conj(normalisation_wh(n, t, c)) * normalisation_wh(0, t, c)
+normalisation(n, t, c)
