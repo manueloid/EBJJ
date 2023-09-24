9224cc9e6b39359f4cd51badc487da5acecf82d4
manueloid <manuel.odelli@gmail.com>
2023-09-24 14:10:28 +0100

    Relation holds for actual values
    Tested if the relations of complex numbers hold for the actual values of
    α.
    The tests have been passed, so now it is time to move to test the
    Fourier transform.

--- test/wavefunction_tests/single_parameter_test.jl
+++ test/wavefunction_tests/single_parameter_test.jl
@@ -39,8 +39,20 @@ $$
 
 As I said I am going to do that numerically, first I will pass some random complex number to see if the relations are correct and only then I will pass the actual values of $ \alpha^2 $ for different times $t$.
 =#
+#=
+## 1 - Testing the relations with random complex numbers
+
+Here I am going to define some functions to test the relations I discussed earlier, but in this case the argument will be only a general complex number.
+
+The plan is to test the relation for a certain number of general complex number and make sure that the relation holds.
+=#
 
 using Test
+"""
+    test_1(param::ComplexF64)
+Given a complex number `param`, this function tests if the relation (4) holds.
+The relation is 2 ℜ(α) / α - 1 = (α*) / α
+"""
 function test_1(param::ComplexF64)
     r = sqrt(real(param))
     result = 2 * r^2 / param - 1 - (conj(param) / param)
@@ -51,9 +63,13 @@ end
         @test isapprox(test_1(), 0.0, atol=1e-10)
     end
 end
+"""
+    test_2(param::ComplexF64)
+Given a complex number `param`, this function tests if the relation (5) holds.
+The relation is α^2 * sqrt(2 * R_α^2 / α^2 - 1) = |α^2|
+"""
 function test_2(param::ComplexF64)
-    r = sqrt(real(param))
-    result = r / (param * sqrt(conj(param)/param)) - r/abs(param)
+    result = (param * sqrt(conj(param)/param)) - abs(param)
     return result
 end
 @testset "testing relation 2" begin
@@ -63,7 +79,38 @@ end
     end
 end
 
+#=
+## 2 - Testing the relations with the actual values of α^2
 
+Now it is time to pass the actual values of $ \alpha^2 $ and see if the relations hold.
+There is no need to define any new functions, but only to get the value of $ \alpha^2 $ at a certain time $ t $ and then pass it to the functions I defined earlier.
+
+The function α2 is the one that returns the value of $ \alpha^2 $ at a certain time $ t $.
+This function will also accept some control parameter variable that are the ones that give the parameters of the system.
+
+I would like to point out that in this case I did not make the substitution $ \hbar \rigtarrow 1 / N$.
+=#
+
+function α2(t::Float64, c::Control)
+    J0, N, U = c.J0, c.N, c.U
+    h = 1 / N
+    b(t) = auxiliary(t, c) # Auxiliary function
+    db(t) = ForwardDiff.derivative(b, t) # Derivative of the auxiliary function
+    α(t::Float64) = ( sqrt(8J0 / (U * N)) / b(t)^2 - 2im / (U * N) * db(t) / b(t) ) / h 
+    return α(t)
+end
+c = ControlFull()
+t = range(0.0, c.T, length=10000)
+@testset "testing relation 1" begin
+    for i in t
+        @test isapprox(test_1(α2(i, c)), 0.0, atol=1e-10)
+    end
+end
+@testset "testing relation 2" begin
+    for i in t
+        @test isapprox(test_2(α2(i, c)), 0.0, atol=1e-10)
+    end
+end
 
 
 
