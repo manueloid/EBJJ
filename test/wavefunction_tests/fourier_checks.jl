88cf5fbca74ac7901bd212e71267f7ba52c08cac
manueloid <manuel.odelli@gmail.com>
2023-09-25 11:04:39 +0100

    Fourier checks passed
    I numerically checked that the analytic solution of the Fourier
    transform of the product of a Gaussian term and the Hermite polynomial
    is correct.
    
    The only thing I need to check now is if the relation still holds when I
    use z / h instead of only z.

--- test/wavefunction_tests/fourier_checks.jl
+++ test/wavefunction_tests/fourier_checks.jl
@@ -1,33 +1,98 @@
-using QuadGK
-using Interpolations
-using BenchmarkTools
-using ForwardDiff
-using SpecialPolynomials
-using HCubature
-using EBJJ
-using Test
-
 #=
 # Fourier checks
-In this set of tests I am going to numerically check if the simplifications I made to turn the STA wave function from momentum to position representation are correct.
 
-I have already checked in [the relevant repository](~/Repos/ExternalBJJ/fourier_transform.wlnb) that the Fourier transform of the product of a Gaussian function and of a Hermite polynomial has a specific close form.
+In this file I am going to numerically check if the analytic form of the Fourier transform of the STA wave function is correct.
+The plan is to numerically implement the Fourier transform and then pass the same value to the analytic form of the Fourier transform and check if the two are equal.
 
-Here I only want to check if the normalisation and the orthogonality hold.
-This can be done by considering the fact that the Fourier transform of the whole STA wave function in momentum representation is the product of the Fourier transform of the spatial part and of the time dependent part.
+I will first do that for a general complex number and only later I will pass the actual value of the parameter $ \alpha^2$.
 
-Hence the STA wave function in position representation is just the product of the time dependent term and the Fourier transform of term dependent on the momentum varible $p$.
-
+I already tested that the analytic solution can be expressed in terms of only on parameter $ \alpha^2$ and its real part $ R_\alpha$.
+I will use this fact to simplify the code.
 =#
 #=
 ### 1 Fourier transform general complex number
-I want to start from the general case, where the parameter $ \eta $ is a complex number and it is not tied to the specific of the system.
-For the moment I do not want to be particularly efficient, I just want to check that the normalisation and the orthogonality hold.
 
-I will thus define the general STA wave function and then test the normalisation for different $ n $.
-I an not going to include the time dependent term, as it is not relevant for the normalisation.
+The function I need to find the Fourier transform of is the following:
+$$
+    \exp\left\{	- \frac{p^2}{2} \alpha^2\right\}
+    \mathcal{H}_n\left( p R_/alpha \right)
+    \tag{1}
+$$
 
+where in this case the parameter $ \alpha $ is not dependent on time $ t $, this will be discussed later.
+In the context of quantum mechanics, the Fourier transform is quite different as we need an extra term $ \hbar$ in the exponent and in the normalisation term.
+In particular, the integral is
+$$ 
+	\frac{1}{\sqrt{2\pi\hbar}}
+	\int_{-\infty}^{+\infty}
+    \exp\left\{	- \frac{p^2}{2} \alpha^2\right\}
+    \mathcal{H}_n\left( p R_/alpha \right)
+	e^{\frac{i}{\hbar}px} dp
+    \tag{2}
+$$
+
+And we can simplify this expression by defining a new variable  $ \tilde{z} = z / \hbar$.
+
+The analytic solution of this integral is given by
+$$
+    \frac{i^n}{\sqrt{\hbar}} 
+    \frac{1}{\alpha}
+    \left[\frac{\alpha^{2*}}{\alpha^2}\right]^{n/2}
+    \exp\left\{- \frac{\tilde{z}^2}{2\alpha^2}\right\}
+    \mathcal{H}_n\left( \tilde{z} \frac{R_\alpha}{|\alpha^2} \right)
+    \tag{3}
+$$
+
+I am going to test this relation for the new defined variable $ \tilde{z} $, I can always pass the actual value of $ z $ later.
+
+#### 1.1 Code implementation
+
+In terms of the implementation of the code, I will define two versions of each term of the function.
+One will be the numerical solution to the integral and I will add the suffix `_num`, while the other one will be the analytical solution of the integral and I will use the suffix `_an`.
+
+I will only care about the Gaussian term and the Hermite polynomial, as the other terms do not depend on the variable $ z $.
+Moreover, I will only define the full function, I will not split the whole function in smaller chunks.
+This is because this file is only a test and I do not need any optimisation.
 =#
+
+he(n::Int64, z::Float64) = SpecialPolynomials.basis(Hermite, n)(z)
+"""
+    toint(n::Int64, p::Float64, α2::ComplexF64)
+This function is the integrand of the Fourier transform of the STA wave function.
+It is basically only the product of the Gaussian term and the Hermite polynomial.
+The function return the value of the integrand for the momentum `p` and for the parameter `α2`, given the excitation level `n`.
+Is the one I am going to pass to the `quadgk` function.
+"""
+function toint(n::Int64, p::Float64, α2::ComplexF64)
+    r = sqrt(real(α2))
+    return exp(-p^2 * α2 / 2) * he(n, p * r)
+end
+"""
+    fourier(n::Int64, z::Float64, α2::ComplexF64)
+Numerically return the value of the Fourier transform of `toint` for the excitation level `n`, the position `z` and the parameter `α2`.
+It also takes the value of the parameter `h` which is the Planck constant.
+"""
+function fourier_num(n::Int64, z::Float64, α2::ComplexF64, h::Float64)
+    return quadgk(p -> toint(n, p, α2) * exp(im * p * z), -Inf, Inf, atol=1e-7)[1] / sqrt(2 * π * h)
+end
+"""
+    fourier_an(n::Int64, z::Float64, α2::ComplexF64, h::Float64)
+Return the value of the analytic solution of the Fourier transform of the STA wave function for the excitation level `n`, the position `z` and the parameter `α2`.
+"""
+function fourier_an(n::Int64, z::Float64, α2::ComplexF64, h::Float64)
+    r = sqrt(real(α2))
+    return im^n / sqrt(h) * 1 / sqrt(α2) * (conj(α2) / α2)^(n / 2) * exp(-z^2 / (2 * α2)) * he(n, z * r / abs(α2))
+end
+@testset "Fourier transform general complex" begin
+    for _ in 1:10000
+        n = rand(0:4)
+        z = rand()
+        α2 = rand(ComplexF64)
+        h = 0.01
+        @test isapprox(fourier_num(n, z, α2, h), fourier_an(n, z, α2, h), atol=1e-4)
+    end
+end
+
 
 normalisation(η::ComplexF64, n::Int64) = (real(η) / pi)^(1 / 4) / sqrt(2^n * factorial(n)) * im^n * sqrt(conj(η) / η)^n / sqrt(η)
 gaussian(z::Float64, η::ComplexF64) = exp(-z^2 / (2 * η))
@@ -37,9 +102,9 @@ spatial_fourier(n::Int64, z::Float64, η::ComplexF64) = normalisation(η, n) * g
 @testset "normalisation STA position general" begin
     η = rand(ComplexF64)
     k = rand(Float64)
-    f(k,n) = exp( im * (n + 1 / 2) * k)
+    f(k, n) = exp(im * (n + 1 / 2) * k)
     for n in 0:5
-        @test isapprox(quadgk(x -> conj(f(k,n)*spatial_fourier(n, x, η)) *f(k,n)* spatial_fourier(n, x, η), -Inf, Inf, atol=1e-7)[1], 1.0, atol=1e-3)
+        @test isapprox(quadgk(x -> conj(f(k, n) * spatial_fourier(n, x, η)) * f(k, n) * spatial_fourier(n, x, η), -Inf, Inf, atol=1e-7)[1], 1.0, atol=1e-3)
     end
 end
 
@@ -74,7 +139,7 @@ function wave_function(n::Int64, t, x, c::Control)
     α(t::Float64) = 2 / b(t)^2 * sqrt(2J0 * N / U) - im * db(t) / (U * b(t)) # Parameter of the Gaussian term
     imag_phase_integrand(t::Float64) = sqrt(2J0 * N * U) / b(t)^2
     φ(t::Float64) = quadgk(τ -> imag_phase_integrand(τ), 0.0, t)[1]
-    return exp(-im * (n + 1/2) * φ(t)) * spatial_fourier(n, x, α(t))
+    return exp(-im * (n + 1 / 2) * φ(t)) * spatial_fourier(n, x, α(t))
 end
 @testset "normalisation with system parameters" begin
     c = ControlFull(10, 0.03)
@@ -85,7 +150,7 @@ end
 end
 @testset "Orthogonalisation with system parameters" begin
     c = ControlFull(10, 0.03)
-    orth(n,m,t) = quadgk(x -> conj(wave_function(n, t, x, c)) * wave_function(m, t, x, c), -Inf, Inf, atol=1e-7)[1] |> real
+    orth(n, m, t) = quadgk(x -> conj(wave_function(n, t, x, c)) * wave_function(m, t, x, c), -Inf, Inf, atol=1e-7)[1] |> real
     for _ in 1:1000
         t, n, m = rand(0.0:c.T), rand(0:3), rand(4:7)
         if n == m
@@ -94,7 +159,7 @@ end
             @test isapprox(orth(n, m, t), 0.0, atol=1e-3)
         end
     end
-end    
+end
 
 #=
 ### 3 Splitting all the stuff
