15126f5440f2672919b86021fc6a4aa38dc11f70
manueloid <manuel.odelli@gmail.com>
2024-06-20 12:28:43 +0100

    Found computational hiccup
    It seemed like the angle for maximum sqeezing for the STA and eSTA
    protocols changed sign a lot so I added an extra check to get the
    minimum of the squeezing for alpha and alpha + pi / 2.
    
    Since we agreed on only evaluating the Ueda squeezing, I am going to
    remove all the parts where Jx is evaluated, to save some time.

--- plots/squeezing.jl
+++ plots/squeezing.jl
@@ -4,7 +4,8 @@ function ΔJ_oat(ψ::Ket, q::ConstantQuantity)
     num = ψ' * (Jz * Jy + Jy * Jz) * ψ
     den = ψ' * Jz^2 * ψ - ψ' * Jy^2 * ψ
     α = 1 / 2 * atan(num / den) |> real
-    return cos(α)^2 * ψ' * Jz^2 * ψ + sin(α)^2 * ψ' * Jy^2 * ψ + cos(α) * sin(α) * ψ' * (Jz * Jy + Jy * Jz) * ψ
+    J(α) = cos(α)^2 * ψ' * Jz^2 * ψ + sin(α)^2 * ψ' * Jy^2 * ψ + cos(α) * sin(α) * ψ' * (Jz * Jy + Jy * Jz) * ψ |> real
+    return min(J(α), J(α + π / 2))
 end
 function css_oat(q::ConstantQuantity, c::Control)
     in = q.css # Initial state
@@ -24,8 +25,8 @@ function gs_oat(q::ConstantQuantity, c::Control)
     Jx = timeevolution.schroedinger_dynamic([0.0, c.T], in, H; fout=ΔJx)[2][end]
     return ξN, Jx
 end
-function css_ibjj(q::ConstantQuantity, c::ControlSTA)
-    in = q.css
+function gs_ibjj(q::ConstantQuantity, c::ControlSTA)
+    in = q.ψ0
     Ω(t) = control_function(t, c)
     H(t, psi) = -2 * Ω(t) * q.Jx + c.U * q.Jz^2
     ΔJ(t, psi) = ΔJ_oat(psi, q) |> real
@@ -34,7 +35,7 @@ function css_ibjj(q::ConstantQuantity, c::ControlSTA)
     Jx = timeevolution.schroedinger_dynamic([0.0, c.T], in, H; fout=ΔJx)[2][end]
     return ξN, Jx
 end
-function gs_ibjj(q::ConstantQuantity, c::ControlFull, corrs::Vector{Float64})
+function gs_ibjjX(q::ConstantQuantity, c::ControlFull, corrs::Vector{Float64})
     in = q.ψ0
     Ω(t) = control_functionX(t, c, corrs)
     H(t, psi) = -2 * Ω(t) * q.Jx + c.U * q.Jz^2
@@ -44,9 +45,9 @@ function gs_ibjj(q::ConstantQuantity, c::ControlFull, corrs::Vector{Float64})
     Jx = timeevolution.schroedinger_dynamic([0.0, c.T], in, H; fout=ΔJx)[2][end]
     return ξN, Jx
 end
-function gs_ibjj(q::ConstantQuantity, c::ControlSTA; expectation::Bool=true)
+function gs_ibjjX(q::ConstantQuantity, c::ControlSTA) 
     in = q.ψ0
-    Ω(t) = control_function(t, c)
+    Ω(t) = control_functionX(t, c)
     H(t, psi) = -2 * Ω(t) * q.Jx + c.U * q.Jz^2
     ΔJ(t, psi) = ΔJ_oat(psi, q) |> real
     ΔJx(t, psi) = (dagger(psi) * (q.Jx)^2 * psi) |> real
@@ -59,13 +60,14 @@ todecibel(x) = 10 * log10(x)
 # just add the calculation for the non X version of STA
 
 using EBJJ, Plots, DelimitedFiles
-t0, tf = 0.05, 1.0
-tfs = range(t0, tf, length=102)
-N = 50
-U, Ωf = 0.1, 0.1
+N = 400
+Λ0 = 2.5
+U, Ωf = 2Λ0 / N, 0.1
+t0, tf = 0.0001/U, 0.005 / U
+tfs = range(t0, tf, length= 52)
 # plot_name = "/home/manueloid/Desktop/sqN_" * string(U) * "_" * string(U * N / (2*Ωf)) * ".png"
 
-function squeezing(c::Control, tfs=AbstractVector{Float64}; Jx::Bool=false)
+function squeezing(c::Control, tfs)
     q = ConstantQuantity(c)
     ξs = zeros(length(tfs), 5)
     Jx = zeros(length(tfs), 5)
@@ -76,9 +78,9 @@ function squeezing(c::Control, tfs=AbstractVector{Float64}; Jx::Bool=false)
         cs = ControlSTA(cl)
         ξs[index, 1], Jx[index, 1] = css_oat(q, cl)
         ξs[index, 2], Jx[index, 2] = gs_oat(q, cl)
-        ξs[index, 3], Jx[index, 3] = css_ibjj(q, cs)
-        ξs[index, 4], Jx[index, 4] = gs_ibjj(q, cs)
-        ξs[index, 5], Jx[index, 5] = gs_ibjj(q, cl, corrs)
+        ξs[index, 3], Jx[index, 3] = gs_ibjj(q, cs)
+        ξs[index, 4], Jx[index, 4] = gs_ibjjX(q, cs)
+        ξs[index, 5], Jx[index, 5] = gs_ibjjX(q, cl, corrs)
         next!(p)
     end
     return ξs / (c.N / 4), Jx
@@ -89,27 +91,6 @@ function squeezing(U::Float64, Ωf::Float64, tfs=AbstractVector{Float64})
     return squeezing(c, tfs)
 end
 ξN, Jx = squeezing(U, Ωf, tfs)
-
-Us = [0.1, 0.2, 0.4]
-Ωfs = [0.1, 0.01]
-for u in Us
-    for σ in Ωfs
-        ξN, Jx = squeezing(U, Ωf, tfs)
-        data_name_ξN = "/home/manueloid/Desktop/Ueda_" * string(U) * "_" * string(U * N / (2 * Ωf)) * ".dat"
-        data_name_Jx = "/home/manueloid/Desktop/Wineland_" * string(U) * "_" * string(U * N / (2 * Ωf)) * ".dat"
-        sqs = todecibel.(ξN * (N / 4)) ./ Jx
-        writedlm(data_name_ξN, ξN)
-        writedlm(data_name_Jx, sqs)
-    end
-end
-
-filename_ξN(U, Ωf) = "/home/manueloid/Desktop/Ueda_" * string(U) * "_" * string(U * N / (2 * Ωf)) * ".dat"
-filename_Jx(U, Ωf) = "/home/manueloid/Desktop/Wineland_" * string(U) * "_" * string(U * N / (2 * Ωf)) * ".dat"
-U, Ωf = 0.1, 0.1
-ξN = readdlm(filename_ξN(U, Ωf))
-Jx = readdlm(filename_Jx(U, Ωf))
-
-sqs = todecibel.(ξN * (N / 4)) ./ Jx
 
 using PGFPlotsX, Colors
 colors = (
@@ -126,22 +107,20 @@ styles = (
     dash=" dashed",
     dot="dash pattern={on 1pt off 1pt}",
 )
-common_style = @pgf {vertical_sep = "0pt", xticklabels_at = "edge bottom", xlabels_at = "edge bottom"}
+common_style = @pgf {vertical_sep = "0pt", xticklcbels_at = "edge bottom", xlabels_at = "edge bottom"}
 esta_opt = @pgf {color = colors.red, line_width = 1, style = styles.solid}
 sta_opt = @pgf{color = colors.black, line_width = 1, style = styles.dash}
 css_oat_style = @pgf {color = colors.green, line_width = 1, style = styles.dot_dash}
-css_ibjj_style = @pgf {color = colors.yellow, line_width = 1, style = styles.ldash}
+staX_opt = @pgf {color = colors.yellow, line_width = 1, style = styles.ldash}
 gs_oat_style = @pgf {color = colors.blue, line_width = 1, style = styles.dot}
-
 #=
 Breakdown of colors
 - black: STA index 4
 - red: eSTA, index 5
 - blue: ψ0 with OAT, index 2
 - green: CSS with OAT, index 1
-- yellow: CSS with IBJJ index 3
+- yellow: STA non X, index 3
 =#
-
 PGFPlotsX.enable_interactive(false)
 canvas = @pgf Axis({
         name = "canvas",
@@ -157,19 +136,20 @@ canvas = @pgf Axis({
         },
         # try_min_ticks = 3,
         max_space_between_ticks = "40pt",
-        xmax = 0.04,
+        # xmin = 0.01, 
         ymax = 0.6,
-        # xtick_distance = 0.006 /4,
+        # xtick_distance = 0.006 /4
         enlarge_x_limits = "false",
         enlarge_y_limits = "0.01",
     },
     {}, Plot(esta_opt, Table(tfs * U, ξN[:, 5])),
-    {}, Plot(sta_opt, Table(tfs * U, ξN[:, 4])),
+    {}, Plot(staX_opt, Table(tfs * U, ξN[:, 4])),
     {}, Plot(css_oat_style, Table(tfs * U, ξN[:, 1])),
     {}, Plot(gs_oat_style, Table(tfs * U, ξN[:, 2])),
-    # {}, Plot(css_ibjj_style, Table(tfs * U, ξN[:, 3])),
+    {}, Plot(sta_opt, Table(tfs * U, ξN[:, 3])),
+    "\\node at (rel axis cs:.8,0.75) {\$ \\Lambda_0 = $( N * U / 2) \$};"
 )
-plot_name = "/home/manueloid/Desktop/Ueda_zoomX" * string(U) * "_" * string(U * N / (2 * Ωf)) * ".pdf"
+plot_name = "/home/manueloid/Desktop/Ueda_" * string(round(U, digits=2)) * "_" * string(U * N / (2 * Ωf)) * ".pdf"
 display(plot_name, canvas)
 
 sqs = todecibel.(ξN) ./ Jx
